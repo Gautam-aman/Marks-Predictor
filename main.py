007089b61a24ba8d733aaa4876e73cc206975832
Aman Gautam <amangautam2128@gmail.com>
2025-11-10 21:42:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error , mean_absolute_error , root_mean_squared_error
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
